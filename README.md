dcc9405e8255a762fb329c5ff537368930fc21a2
mahimcbu <101466400+mahimcbu@users.noreply.github.com>
2022-12-08 14:41:41 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mahim.github.io
+
+This is a 7 page portfolio website made for BU software development CS601 course final project.
+I built it using the first three assignments and included 7 pages which was recommended by the facilitator to us at the biginning. 
+
+I have validated all page html, css, js, and JSON files using the appropriate validator and linter. Also, I added the NoScript for any situation where the JS can't work.
+
+I have used 4 different media queries in the css to fit the need of all type of screen size. I used flexbox, css grid, animation,
+etc. in the css to make the website responsive and !dull.
+
+On the feedback page, I used vue to perform form submission with validation. I also used a bit of vue framework in the education page.
+in the same page, I used fetch api to fetch some json objects(my college degrees) and display them after a button is clicked. 
+
+I tried to maintain a consistent navbar, color combination, usage of html 5 syntaxes throughout the 7 page application.
+
+I am a complete beginner here so please don't hesitate to provide your feedback.
--- Script/app.js
+++ Script/app.js
@@ -1,12 +1,21 @@
-// const one = new Vue({
-//     el: '#app',
-//     data: () => {
-//         return {
-//         sbusrc: "https://patch.com/img/cdn/users/21766/2012/02/raw/f67ef214a6fcfcb64a7565d2281e6b18.jpg",
-//         busrc: "https://logos-download.com/wp-content/uploads/2020/06/Boston_University_Logo_text.png"
-//         }
-//     }
-// })
+const one = new Vue({
+    el: '#app',
+    data: () => {
+        return {
+        sbusrc: "https://patch.com/img/cdn/users/21766/2012/02/raw/f67ef214a6fcfcb64a7565d2281e6b18.jpg",
+        busrc: "https://logos-download.com/wp-content/uploads/2020/06/Boston_University_Logo_text.png"
+        }
+    }
+})
+
+export default one = new Vue({
+    el: '#app',
+    data () {
+        return{
+            
+        }
+    }
+})
 console.log("Vue loaded")
 const two = new Vue({
     el: '#appform',
@@ -28,16 +37,6 @@ const two = new Vue({
     },
     
     methods: {
-        // fnameValid(){
-        //     let regex = /^[A-Za-z]+$/;
-        //     return ((this.firstName.match(regex)) && (this.firstName.length >2));
-          
-           
-        // },
-        // lnameValid(){
-        //     let regex = /^[A-Za-z]+$/;
-        //     return ((this.lastName.match(regex)) && (this.lastName.length >2));
-        // },
 
         validateForm(){
             
